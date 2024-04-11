e0658de61b240ab5a7e94c800649ba48cc693594
Fernanda Dias <fernandadias9@hotmail.com>
2024-04-11 14:19:46 -0300

    Separação em pastas back e front-end, início do projeto front-end em Angular

---
+++ back-end/src/main/java/controller/PaisController.java
@@ -0,0 +1,32 @@
+package controller;
+
+import exception.ControleVacinasException;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import model.entity.Pais;
+import service.PaisService;
+
+@Path("/pais")
+public class PaisController {
+
+private PaisService paisService = new PaisService();
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Pais cadastrar(Pais novoPais) throws ControleVacinasException {
+		 return paisService.cadastrar(novoPais);
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/buscar/{id}")
+	public Pais buscar(@PathParam("id") int id){
+		 return paisService.buscar(id);
+	}
+}
